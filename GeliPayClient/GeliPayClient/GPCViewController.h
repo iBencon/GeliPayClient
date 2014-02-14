a3947ada34ee1afc477319c4f7ccd9d7b338174d
西山 勇世 <nishiyama_y@mti.co.jp>
2014-02-14 09:43:49 +0900

    Create Xcode project.

---
+++ GeliPayClient/GeliPayClient/GPCViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPCViewController.h
+//  GeliPayClient
+//
+//  Created by 西山 勇世 on 2014/02/14.
+//  Copyright (c) 2014年 iBencon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPCViewController : UIViewController
+
+@end
---
+++ GeliPayClient/GeliPayClient/GPCViewController.m
@@ -0,0 +1,29 @@
+//
+//  GPCViewController.m
+//  GeliPayClient
+//
+//  Created by 西山 勇世 on 2014/02/14.
+//  Copyright (c) 2014年 iBencon. All rights reserved.
+//
+
+#import "GPCViewController.h"
+
+@interface GPCViewController ()
+
+@end
+
+@implementation GPCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
